7bfccc2dce3dba358f935687e92006ee530dd677
Jasper Van der Jeugt <m@jaspervdj.be>
2020-12-17 14:42:30 +0100

    2020/17: a little faster

--- 2020/17.hs
+++ 2020/17.hs
@@ -1,31 +1,39 @@
-{-# LANGUAGE DataKinds      #-}
-{-# LANGUAGE KindSignatures #-}
-import qualified AdventOfCode.Grid   as Grid
-import           AdventOfCode.Main   (simpleMain)
-import           AdventOfCode.V2     (V2 (..))
-import           Control.Monad       (guard)
-import qualified Data.Map            as Map
-import           Data.Set            (Set)
-import qualified Data.Set            as Set
-import qualified Data.Vector.Unboxed as VU
-import           GHC.TypeLits        (Nat)
+import qualified AdventOfCode.Grid as Grid
+import           AdventOfCode.Main (simpleMain)
+import           AdventOfCode.V2   (V2 (..))
+import qualified Data.Map          as Map
+import           Data.Set          (Set)
+import qualified Data.Set          as Set
 
-type Cubes (n :: Nat) = Set (Vec n)
-newtype Vec (n :: Nat) = Vec (VU.Vector Int) deriving (Eq, Ord)
+data Vec3 = Vec3
+    {-# UNPACK #-} !Int
+    {-# UNPACK #-} !Int
+    {-# UNPACK #-} !Int
+    deriving (Eq, Ord)
 
-vec3 :: V2 Int -> Vec 3
-vec3 (V2 x y) = Vec $ VU.fromList [x, y, 0]
+traverseVec3 :: Applicative f => (Int -> f Int) -> Vec3 -> f Vec3
+traverseVec3 f (Vec3 x y z) = Vec3 <$> f x <*> f y <*> f z
 
-vec4 :: V2 Int -> Vec 4
-vec4 (V2 x y) = Vec $ VU.fromList [x, y, 0, 0]
+data Vec4 = Vec4
+    {-# UNPACK #-} !Int
+    {-# UNPACK #-} !Int
+    {-# UNPACK #-} !Int
+    {-# UNPACK #-} !Int
+    deriving (Eq, Ord)
 
-neighbours :: Vec n -> [Vec n]
-neighbours (Vec vec) = do
-    delta <- VU.replicateM (VU.length vec) [-1, 0, 1]
-    guard . not $ VU.all (== 0) delta
-    pure . Vec $ VU.zipWith (+) vec delta
+traverseVec4 :: Applicative f => (Int -> f Int) -> Vec4 -> f Vec4
+traverseVec4 f (Vec4 x y z w) = Vec4 <$> f x <*> f y <*> f z <*> f w
 
-step :: Cubes n -> Cubes n
+class TraversableVec t where
+    traverseVec :: Applicative f => (Int -> f Int) -> t -> f t
+
+instance TraversableVec Vec3 where traverseVec = traverseVec3
+instance TraversableVec Vec4 where traverseVec = traverseVec4
+
+neighbours :: (TraversableVec t, Eq t) => t -> [t]
+neighbours x = filter (/= x) $ traverseVec (\i -> [i, i - 1, i + 1]) x
+
+step :: (TraversableVec t, Ord t) => Set t -> Set t
 step active0 =
     Set.filter isActive . Set.fromList . concatMap neighbours $
     Set.toList active0
@@ -38,7 +46,9 @@ step active0 =
 
 main :: IO ()
 main = simpleMain $ \inputstr ->
-    let grid2d = Map.keysSet . Map.filter (== '#') $ Grid.fromString inputstr in
-    ( Set.size $ iterate step (Set.map vec3 grid2d) !! 6
-    , Set.size $ iterate step (Set.map vec4 grid2d) !! 6
+    let grid2d = Map.keysSet . Map.filter (== '#') $ Grid.fromString inputstr
+        toVec3 (V2 x y) = Vec3 x y 0
+        toVec4 (V2 x y) = Vec4 x y 0 0 in
+    ( Set.size $ iterate step (Set.map toVec3 grid2d) !! 6
+    , Set.size $ iterate step (Set.map toVec4 grid2d) !! 6
     )
