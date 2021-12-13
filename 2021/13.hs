4c60da9e33d37392a3b1f6d30384fcce2fdbdc61
Jasper Van der Jeugt <m@jaspervdj.be>
2021-12-13 15:51:14 +0100

    2021/13: Cleanup

--- 2021/13.hs
+++ 2021/13.hs
@@ -5,11 +5,12 @@ import           AdventOfCode.Main       (pureMain)
 import qualified AdventOfCode.NanoParser as P
 import           AdventOfCode.V2         (V2 (..))
 import           Control.Applicative     ((<|>))
-import           Data.Foldable           (foldl')
+import           Data.Coerce             (coerce)
 import qualified Data.Map                as M
+import           Data.Monoid             (Dual (..), Endo (..))
 import qualified Data.Set                as S
 
-data Fold = X Int | Y Int deriving (Show)
+type Fold  = Dual (Endo (V2 Int))
 type Sheet = S.Set (V2 Int)
 
 parseInput :: P.Parser Char (Sheet, [Fold])
@@ -17,14 +18,14 @@ parseInput = (,) <$> (S.fromList <$> P.many1 v2) <*> P.many1 fold
   where
     v2   = V2 <$> (P.decimal <* P.char ',') <*> (P.decimal <* P.spaces)
     fold = P.string "fold along " *> xy <* P.spaces
-    xy   = ((X <$ P.string "x=") <|> (Y <$ P.string "y=")) <*> P.decimal
+    xy   = ((foldX <$ P.string "x=") <|> (foldY <$ P.string "y=")) <*> P.decimal
 
-foldPoint :: Fold -> V2 Int -> V2 Int
-foldPoint (X xf) (V2 x y) = if x > xf then V2 (2 * xf - x) y else V2 x y
-foldPoint (Y yf) (V2 x y) = if y > yf then V2 x (2 * yf - y) else V2 x y
+foldX, foldY :: Int -> Fold
+foldX xf = coerce $ \(V2 x y) -> if x > xf then V2 (2 * xf - x) y else V2 x y
+foldY yf = coerce $ \(V2 x y) -> if y > yf then V2 x (2 * yf - y) else V2 x y
 
 foldSheet :: Fold -> Sheet -> Sheet
-foldSheet f = S.map (foldPoint f)
+foldSheet = S.map . coerce
 
 sheetToString :: Sheet -> String
 sheetToString s = G.toString $ M.fromList [(p, '#') | p <- S.toList s]
@@ -32,6 +33,6 @@ sheetToString s = G.toString $ M.fromList [(p, '#') | p <- S.toList s]
 main :: IO ()
 main = pureMain $ \input -> do
     (sheet, folds) <- P.runParser parseInput input
-    let part1 = foldSheet (head folds) sheet
-        part2 = foldl' (flip foldSheet) sheet folds
+    let part1 = foldSheet (head folds)    sheet
+        part2 = foldSheet (mconcat folds) sheet
     pure (pure (S.size part1), pure (sheetToString part2))
