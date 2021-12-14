d65c36063a62b48ce234c916369378dde92b1237
Jasper Van der Jeugt <m@jaspervdj.be>
2021-12-14 15:39:53 +0100

    2021/14: Cleanup, unify times & single

--- 2021/14.hs
+++ 2021/14.hs
@@ -16,9 +16,9 @@ type Rules a = M.Map (Pair a) a
 -- Count frequencies for some type
 newtype Freqs a = Freqs {unFreqs :: M.Map a Integer} deriving (Show)
 
--- Just a single item is a frequency of 1
-single :: Ord a => a -> Freqs a
-single x = Freqs (M.singleton x 1)
+-- A single item, n times.
+times :: Integer -> a -> Freqs a
+times k x = Freqs (M.singleton x k)
 
 -- We can add frequencies together
 instance Ord a => Semigroup (Freqs a) where
@@ -27,18 +27,14 @@ instance Ord a => Semigroup (Freqs a) where
 -- Zero frequencies
 instance Ord a => Monoid (Freqs a) where mempty = Freqs M.empty
 
--- We can multiply frequencies
-times :: Integer -> Freqs a -> Freqs a
-times k (Freqs m) = Freqs ((* k) <$> m)
-
 -- If we have frequencies of collections, we can take the frequencies of the
 -- individual items.
 separate :: (Foldable f, Ord a) => Freqs (f a) -> Freqs a
-separate = M.foldMapWithKey (\f n -> foldMap (times n . single) f) . unFreqs
+separate = M.foldMapWithKey (\f n -> foldMap (times n) f) . unFreqs
 
 -- You can construct frequencies of pairs from the adjacent elements of a list
 initial :: Ord a => [a] -> Freqs (Pair a)
-initial l = mconcat $ zipWith (\x y -> single $ Pair x y) l (drop 1 l)
+initial l = mconcat $ zipWith (\x y -> times 1 (Pair x y)) l (drop 1 l)
 
 -- A collection of rules is a function of frequencies of pairs to new
 -- frequencies of pairs
@@ -46,31 +42,18 @@ step :: Ord a => Rules a -> Freqs (Pair a) -> Freqs (Pair a)
 step rules (Freqs m) =
     M.foldMapWithKey (\pair@(Pair x y) n -> case M.lookup pair rules of
         -- Keep the existing pairs
-        Nothing -> times n (single pair)
+        Nothing -> times n pair
         -- Introduce new pairs if there is a rule match.
-        Just z  ->
-            times n (single (Pair x z)) <> times n (single (Pair z y))) m
+        Just z  -> times n (Pair x z) <> times n (Pair z y)) m
 
--- Because we want to demonstrate the slow and fast solution, lets use a type
--- that can step N times.
-type Solver a = Int -> Freqs (Pair a) -> Freqs (Pair a)
-
--- We can now write a solver for the puzzle.  We leave out the part that steps
--- N times.
-solve :: Ord a => Solver a -> [a] -> Int -> Integer
-solve solver polymer steps =
-    let freqs = fmap (`div` 2) . unFreqs $
-            single (head polymer) <> single (last polymer) <>
-            separate (solver steps (initial polymer)) in
+-- We can now write a solver for the puzzle.
+solve :: Ord a => Rules a -> [a] -> Int -> Integer
+solve rules polymer n =
+    let steps = appEndo . stimes n . Endo $ step rules
+        freqs = fmap (`div` 2) . unFreqs $
+            times 1 (head polymer) <> times 1 (last polymer) <>
+            separate (steps $ initial polymer) in
     maximum freqs - minimum freqs
-
--- A slow solver just applies 'step' N times.
-slowSolver :: Ord a => Rules a -> Solver a
-slowSolver rules n freqs = iterate (step rules) freqs !! n
-
--- A fast solver uses the semigroup instance of `Endo Frequency`
-fastSolver :: Ord a => Rules a -> Solver a
-fastSolver rules n = appEndo . stimes n . Endo $ step rules
 
 -- Input parser
 parseInput :: P.Parser Char (String, Rules Char)
@@ -87,6 +70,6 @@ parseInput = (,)
 main :: IO ()
 main = pureMain $ \input -> do
     (polymer, rules) <- P.runParser parseInput input
-    let part1 = solve (slowSolver rules) polymer 10
-        part2 = solve (fastSolver rules) polymer 40
+    let part1 = solve rules polymer 10
+        part2 = solve rules polymer 40
     pure (pure part1, pure part2)
