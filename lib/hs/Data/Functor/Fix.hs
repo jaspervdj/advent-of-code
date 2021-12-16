0b9d6b5691d1a5ce1c3abbdf5da48593dbe77368
Jasper Van der Jeugt <m@jaspervdj.be>
2021-12-16 11:57:35 +0100

    2021/16: Remove Show Fix instance

--- lib/hs/Data/Functor/Fix.hs
+++ lib/hs/Data/Functor/Fix.hs
@@ -1,13 +1,9 @@
-{-# LANGUAGE UndecidableInstances #-}
 module Data.Functor.Fix
     ( Fix (..)
     , cata
     ) where
 
 newtype Fix f = Fix {unFix :: f (Fix f)}
-
-instance (Functor f, Show (f String)) => Show (Fix f) where
-    show = cata show
 
 cata :: Functor f => (f a -> a) -> Fix f -> a
 cata f = f . fmap (cata f) . unFix
