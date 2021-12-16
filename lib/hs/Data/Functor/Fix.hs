4e72b37bcf97372a68bc2fc37b95a12f29f95a40
Jasper Van der Jeugt <m@jaspervdj.be>
2021-12-16 12:20:34 +0100

    Data.Functor.Fix: add para

--- lib/hs/Data/Functor/Fix.hs
+++ lib/hs/Data/Functor/Fix.hs
@@ -1,9 +1,13 @@
 module Data.Functor.Fix
     ( Fix (..)
     , cata
+    , para
     ) where
 
 newtype Fix f = Fix {unFix :: f (Fix f)}
 
 cata :: Functor f => (f a -> a) -> Fix f -> a
 cata f = f . fmap (cata f) . unFix
+
+para :: Functor f => (f (Fix f, a) -> a) -> Fix f -> a
+para f = f . fmap (\g -> (g, para f g)) . unFix
