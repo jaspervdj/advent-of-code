6be0ca9bad9223a56072888c8183a04b2a9ee056
Jasper Van der Jeugt <m@jaspervdj.be>
2024-12-10 10:52:28 +0100

    2024/10: merge part1 and part2

--- 2024/10.hs
+++ 2024/10.hs
@@ -1,38 +1,30 @@
-import           AdventOfCode.Dijkstra     (Bfs (..), bfs)
 import qualified AdventOfCode.Grid.Bounded as G
 import           AdventOfCode.Main         (pureMain)
-import           Control.Monad             (guard)
 import           Data.Char                 (digitToInt, isDigit)
+import           Data.Foldable             (fold)
 import qualified Data.Map                  as M
 import           Data.Maybe                (mapMaybe)
+import           Data.Monoid               (Sum (..))
+import qualified Data.Set                  as S
 import           Data.Traversable          (for)
 
-trailheads :: G.Grid Int -> [G.Pos]
-trailheads = map fst . filter ((== 0) . snd) . G.toList
+height :: Int -> G.Grid Int -> [G.Pos]
+height h = map fst . filter ((== h) . snd) . G.toList
 
-score :: G.Grid Int -> G.Pos -> Int
-score grid trailhead = length $ filter (\p -> grid G.! p == 9) $ M.keys $
-    bfsDistances $ bfs
-        (\p -> do
-            n <- filter (`G.member` grid) $ G.neighbours p
-            guard $ grid G.! n == grid G.! p + 1
-            pure n)
-        (const False)
-        trailhead
-
-ratings :: G.Grid Int -> Int
-ratings grid = go 0 $ M.fromList $ zip (trailheads grid) $ repeat 1
+hikes :: Monoid m => (G.Pos -> m) -> G.Grid Int -> M.Map G.Pos m
+hikes f grid = ascend 1 $ M.fromList [(p, f p) | p <- height 0 grid]
   where
-    go h level | h >= 9 = sum level
-    go h level          = go (h + 1) $ M.fromList $ do
-       p <- map fst $ filter ((== h + 1) . snd) $ G.toList grid
-       pure (p, sum $ mapMaybe (`M.lookup` level) $ G.neighbours p)
+    ascend h level | h > 9 = level
+    ascend h level         = ascend (h + 1) $ M.fromList
+       [ (p, mconcat $ mapMaybe (`M.lookup` level) $ G.neighbours p)
+       | p <- height h grid
+       ]
 
 main :: IO ()
 main = pureMain $ \input -> do
     grid0 <- G.fromString input
     grid1 <- for grid0 $ \c ->
         if isDigit c then pure (digitToInt c) else Left $ "bad char: " ++ [c]
-    let part1      = sum $ map (score grid1) (trailheads grid1)
-        part2      = ratings grid1
+    let part1 = getSum $ foldMap (Sum . S.size) $ hikes S.singleton grid1
+        part2 = getSum $ fold $ hikes (\_ -> Sum 1) grid1 :: Int
     pure (pure part1, pure part2)
