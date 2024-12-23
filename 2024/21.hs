7af4293917da7e9f3ca421836e4abd92a5774208
Jasper Van der Jeugt <m@jaspervdj.be>
2024-12-23 16:14:13 +0100

    2024/21: type safe stack

--- 2024/21.hs
+++ 2024/21.hs
@@ -1,7 +1,11 @@
+{-# LANGUAGE DataKinds           #-}
+{-# LANGUAGE GADTs               #-}
+{-# LANGUAGE ScopedTypeVariables #-}
 import qualified AdventOfCode.Grid   as G
 import           AdventOfCode.Main   (pureMain)
 import           AdventOfCode.V2     (V2 (..))
-import           Control.Monad.State
+import           Control.Monad.State (State, gets, modify, runState, state)
+import           Data.Kind           (Type)
 import           Data.List           (foldl')
 import qualified Data.Map            as M
 import           Data.Maybe          (maybeToList)
@@ -9,11 +13,15 @@ import qualified Data.Set            as S
 import           Data.Traversable    (for)
 import           Text.Read           (readMaybe)
 
+-- | Use a dedicated datatype for digits so we can write total rather than
+-- partial functions and have a convenient @Bounded@ instance.
 data N = N0 | N1 | N2 | N3 | N4 | N5 | N6 | N7 | N8 | N9
     deriving (Bounded, Enum, Read, Show)
 
-data K k = A | K k deriving (Eq, Ord, Show)
+-- | Either the enter key (A) or another key (where @i@ stands for input).
+data K i = A | K i deriving (Eq, Ord, Show)
 
+-- | A sequence of digit keys ending with enter (A).
 newtype Sequence = Sequence [N] deriving (Show)
 
 instance Read Sequence where
@@ -21,95 +29,132 @@ instance Read Sequence where
         (digits, 'A' : remainder) -> do
             ns <- traverse (\d -> readMaybe ['N', d]) digits
             pure (Sequence ns, remainder)
-        (_, _) -> Nothing
+        _ -> Nothing
 
 numeric :: Sequence -> Int
 numeric (Sequence ns) = foldl' (\acc n -> acc * 10 + fromEnum n) 0 ns
 
-data Keypad k = Keypad
-    { kpGrid :: S.Set G.Pos
-    , kpPos  :: K k -> G.Pos
-    }
+-- | A list of alternatives keypresses that will accomplish the same thing.
+-- Having this gives some more semantics to the nested lists to improve
+-- readability.
+newtype Alternatives a = Alternatives [a]
+    deriving (Functor, Foldable, Traversable)
 
-mkKeypad :: (Bounded k, Enum k) => (K k -> G.Pos) -> Keypad k
-mkKeypad f = Keypad (S.fromList $ map f keys) f
+-- | A keypad finds buttons to touch to navigate in between two keys.
+-- We can think of @k@ as the "input" to the keyboard (the buttons we want
+-- to press) and @d@ as the output (possible that buttons could be pressed
+-- to achieve this).
+--
+-- Capturing both of these in the type allows us to build type-safe stacks of
+-- keyboards, where each one feeds into the next.
+type Keypad i o = [i] -> [Alternatives [o]]
+
+-- | Generate a keypad that outputs directions, based on a total function that
+-- generates positions for the keys.
+mkKeypad :: forall i. (Bounded i, Enum i) => (K i -> G.Pos) -> Keypad i G.Dir
+mkKeypad pos =
+    -- Start navigating from the enter key and end back there.
+    \s -> zipWith navigate (A : map K s) (map K s ++ [A])
   where
     keys = A : map K [minBound .. maxBound]
+    grid = S.fromList $ map pos keys
 
-numKeypad :: Keypad N
+    navigate :: K i -> K i -> Alternatives [G.Dir]
+    navigate src dst
+        | x0 == x1  = Alternatives [vert]
+        | y0 == y1  = Alternatives [hori]
+        | otherwise = Alternatives $
+            [(hori ++ vert) | V2 x1 y0 `S.member` grid] ++
+            [(vert ++ hori) | V2 x0 y1 `S.member` grid]
+      where
+        V2 x0 y0 = pos src
+        V2 x1 y1 = pos dst
+        hori     = replicate (abs (x0 - x1)) $ if x0 < x1 then G.R else G.L
+        vert     = replicate (abs (y0 - y1)) $ if y0 < y1 then G.D else G.U
+
+numKeypad :: Keypad N G.Dir
 numKeypad = mkKeypad $ \key -> case key of
     K N7 -> V2 0 0; K N8 -> V2 1 0; K N9 -> V2 2 0
     K N4 -> V2 0 1; K N5 -> V2 1 1; K N6 -> V2 2 1
     K N1 -> V2 0 2; K N2 -> V2 1 2; K N3 -> V2 2 2
     K                 N0 -> V2 1 3; A    -> V2 2 3
 
-dirKeypad :: Keypad G.Dir
+dirKeypad :: Keypad G.Dir G.Dir
 dirKeypad = mkKeypad $ \key -> case key of
     K                  G.U -> V2 1 0; A     -> V2 2 0
     K G.L -> V2 0 1; K G.D -> V2 1 1; K G.R -> V2 2 1
 
-newtype Dirs = Dirs [G.Dir] deriving (Eq, Ord)
+-- | A type-safe stack of keyboards.  We use a list to indicate what sort of
+-- buttons are used in each layer.
+data Stack (l :: [Type]) where
+    -- | A single-element stack.  This only contains a keypad.
+    SSingle :: Keypad i o -> Stack '[i, o]
 
-navigate :: Keypad k -> K k -> K k -> [Dirs]
-navigate kp src dst
-    | x0 == x1  = [Dirs vert]
-    | y0 == y1  = [Dirs hori]
-    | otherwise =
-        [Dirs (hori ++ vert) | V2 x1 y0 `S.member` kpGrid kp] ++
-        [Dirs (vert ++ hori) | V2 x0 y1 `S.member` kpGrid kp]
+    -- | A keyboard on top of a stack.  This contains a keypad, a cache, and
+    -- the next stack.
+    SCons
+        :: Ord o
+        => Keypad i o -> M.Map [o] Int -> Stack (o ': t)
+        -> Stack (i ': o ': t)
+
+scons :: Ord o => Keypad i o -> Stack (o ': t) -> Stack (i ': o ': t)
+scons kp = SCons kp mempty
+
+shortest :: forall i t. [i] -> Stack (i ': t) -> (Int, Stack (i ': t))
+shortest keys stack = case stack of
+    SSingle keypad ->
+        let len = sum $ map (succ . minimum . fmap length) $ keypad keys in
+        (len, stack)
+    SCons keypad cache next ->
+        let (len, (cache', next')) = runState
+                (fmap sum $ mapM descend $ keypad keys)
+                (cache, next) in
+        (len, SCons keypad cache' next')
   where
-    V2 x0 y0 = kpPos kp src
-    V2 x1 y1 = kpPos kp dst
-    hori     = replicate (abs (x0 - x1)) $ if x0 < x1 then G.R else G.L
-    vert     = replicate (abs (y0 - y1)) $ if y0 < y1 then G.D else G.U
+    descend
+        :: forall o t'. Ord o
+        => Alternatives [o] -> State (M.Map [o] Int, Stack (o ': t')) Int
+    descend alts = fmap minimum $ for alts $ \dirs -> cached dirs $ do
+        next <- gets snd
+        let (len, next') = shortest dirs next
+        modify $ \(cache, _) -> (cache, next')
+        pure len
 
-data Stack k = Stack
-    { sKeypad :: Keypad k
-    , sCache  :: M.Map Dirs Int
-    , sNext   :: Maybe (Stack G.Dir)
-    }
-
-singleton :: Keypad k -> Stack k
-singleton l = Stack l mempty Nothing
-
-addKeypad :: Keypad k -> Stack G.Dir -> Stack k
-addKeypad l stack = Stack l mempty $ Just stack
-
-shortest :: [k] -> Stack k -> (Int, Stack k)
-shortest keys = runState $ fmap sum $ mapM work $
-    zip (A : map K keys) (map K keys ++ [A])
-  where
+    cached
+        :: forall o t'. Ord o
+        => [o]
+        -> State (M.Map [o] Int, Stack (o ': t')) Int
+        -> State (M.Map [o] Int, Stack (o ': t')) Int
     cached k f = do
-        mbLen <- gets (M.lookup k . sCache)
+        mbLen <- gets (M.lookup k . fst)
         case mbLen of
             Just len -> pure len
             Nothing -> do
                 len <- f
-                modify $ \r -> r {sCache = M.insert k len (sCache r)}
-                pure len
-
-    work (from, to) = do
-        keypad <- gets sKeypad
-        let options = navigate keypad from to
-        mbNext <- gets sNext
-        fmap minimum $ case mbNext of
-            Nothing -> pure [length ds + 1 | Dirs ds <- options]
-            Just next -> for options $ \dirs@(Dirs ds) -> cached dirs $ do
-                let (len, next') = shortest ds next
-                modify $ \r -> r {sNext  = Just next'}
+                modify $ \(cache, next) -> (M.insert k len cache, next)
                 pure len
 
 main :: IO ()
 main = pureMain $ \str -> do
     sequences <- maybe (Left "no parse") pure $ traverse readMaybe $ lines str
 
-    let dirs n = iterate (addKeypad dirKeypad) (singleton dirKeypad) !! (n - 1)
-        stack2  = addKeypad numKeypad $ dirs (2 :: Int)
-        stack25 = addKeypad numKeypad $ dirs (25 :: Int)
+    let stack1 :: Stack '[N, G.Dir, G.Dir, G.Dir]
+        stack1 = scons numKeypad $ scons dirKeypad $ SSingle dirKeypad
+        stack2 = scons numKeypad $
+            scons dirKeypad $ scons dirKeypad $ scons dirKeypad $
+            scons dirKeypad $ scons dirKeypad $ scons dirKeypad $
+            scons dirKeypad $ scons dirKeypad $ scons dirKeypad $
+            scons dirKeypad $ scons dirKeypad $ scons dirKeypad $
+            scons dirKeypad $ scons dirKeypad $ scons dirKeypad $
+            scons dirKeypad $ scons dirKeypad $ scons dirKeypad $
+            scons dirKeypad $ scons dirKeypad $ scons dirKeypad $
+            scons dirKeypad $ scons dirKeypad $ scons dirKeypad $
+            SSingle dirKeypad
 
-        solve stack = flip evalState stack $ fmap sum $
+        solve :: forall t. Stack (N ': t) -> Int
+        solve stack = fst $ flip runState stack $ fmap sum $
             for sequences $ \s@(Sequence ns) -> do
                 len <- state (shortest ns)
                 pure $ numeric s * len
 
-    pure (pure (solve stack2), pure (solve stack25))
+    pure (pure (solve stack1), pure (solve stack2))
